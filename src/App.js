08f9d1deb5e2f2b9fb0178532814337f9df3a742
Zuzanna Usakiewicz <z.usakiewicz@gmail.com>
2021-12-05 21:30:19 +0100

    added app homepage

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
 import "./App.css";
+import React from "react";
+import Navbar from "./components/Navbar";
+import BooksContextProvider from "./contexts/BooksContext";
+import Modal from "./components/Modal";
+import BookList from "./components/BooksList";
+import BookForm from "./components/BookForm";
+import useToggle from "./hooks/useToggle";
+import { BiBookAdd } from "react-icons/bi";
 
 function App() {
-  return <div className="App"></div>;
+  const [value, toggleValue] = useToggle(false);
+  return (
+    <div className="App">
+      <Navbar />
+      <header>
+        <h1>The Librarian</h1>
+      </header>
+
+      <BooksContextProvider>
+        <button className="add-btn" onClick={toggleValue}>
+          add book <BiBookAdd />
+        </button>
+        {value && (
+          <Modal toggleValue={toggleValue}>
+            <BookForm toggleValue={toggleValue} />
+          </Modal>
+        )}
+        <section className="booklist-container">
+          <BookList />
+        </section>
+      </BooksContextProvider>
+    </div>
+  );
 }
 
 export default App;
