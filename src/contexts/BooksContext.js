d3fb97e3d5d072ca6c3725a9161f1338948281f1
Zuzanna Usakiewicz <z.usakiewicz@gmail.com>
2022-01-03 18:20:12 +0100

    add popup function on add, delete and edit book

--- src/contexts/BooksContext.js
+++ src/contexts/BooksContext.js
@@ -4,6 +4,7 @@ import { v4 as uuidv4 } from "uuid";
 export const BooksContext = createContext();
 
 const BooksContextProvider = (props) => {
+  const [popup, setPopup] = useState({ isOn: false, type: null });
   const [allBooks, setAllBooks] = useState([
     {
       id: uuidv4(),
@@ -15,16 +16,32 @@ const BooksContextProvider = (props) => {
   ]);
 
   useEffect(() => {
+    const timer = setTimeout(() => {
+      setPopup({ isOn: false, type: null });
+    }, 3000);
+    return () => clearTimeout(timer);
+  }, [popup]);
+
+  // useEffect(() => {
+  //   const timer = setTimeout(() => {
+  //     setPopupDelete(false);
+  //   }, 3000);
+  //   return () => clearTimeout(timer);
+  // }, [popupDelete]);
+
+  useEffect(() => {
     setAllBooks(JSON.parse(localStorage.getItem("allBooks")));
   }, []);
 
   useEffect(() => {
     localStorage.setItem("allBooks", JSON.stringify(allBooks));
   });
+
   const addBook = (newBook) => {
     setAllBooks((prevBooks) => {
       return [newBook, ...prevBooks];
     });
+    setPopup({ isOn: true, type: "added!" });
   };
   const deleteBook = (id) => {
     setAllBooks((prevBooks) => {
@@ -32,17 +49,25 @@ const BooksContextProvider = (props) => {
         return id !== book.id;
       });
     });
+    setPopup({ isOn: true, type: "deleted!" });
   };
 
   const updateBook = (id, updatedBook) => {
     setAllBooks((prevBooks) => {
       return prevBooks.map((book) => (book.id === id ? updatedBook : book));
     });
+    setPopup({ isOn: true, type: "edited!" });
   };
 
   return (
     <BooksContext.Provider
-      value={{ allBooks, addBook, deleteBook, updateBook }}
+      value={{
+        allBooks,
+        addBook,
+        deleteBook,
+        updateBook,
+        popup,
+      }}
     >
       {props.children}
     </BooksContext.Provider>
