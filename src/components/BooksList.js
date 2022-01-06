66a23df240526640fadbfc713f96f5d7e2a59e03
Zuzanna Usakiewicz <z.usakiewicz@gmail.com>
2022-01-06 20:14:04 +0100

    add pagination and notifications to the booklist component

--- src/components/BooksList.js
+++ src/components/BooksList.js
@@ -1,12 +1,33 @@
-import React, { useContext } from "react";
+import React, { useState, useContext } from "react";
 import Book from "./Book";
+import Notification from "./Notification";
+import Pagination from "./Pagination";
 import { BooksContext } from "../contexts/BooksContext";
 
 export default function BooksList() {
-  const { allBooks } = useContext(BooksContext);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [booksPerPage] = useState(5);
+
+  const { allBooks, notification } = useContext(BooksContext);
+
+  const indexOfLastBook = currentPage * booksPerPage;
+  const indexOfFirstBook = indexOfLastBook - booksPerPage;
+  const currentBooks = allBooks.slice(indexOfFirstBook, indexOfLastBook);
+
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
   return (
     <div className="main-container">
-      {allBooks.map((book) => {
+      {notification.isOn ? (
+        <Notification> {notification.type} </Notification>
+      ) : null}
+
+      <Pagination
+        booksPerPage={booksPerPage}
+        totalBooks={allBooks.length}
+        paginate={paginate}
+        currentPage={currentPage}
+      />
+      {currentBooks.map((book) => {
         return <Book book={book} key={book.id} />;
       })}
     </div>
