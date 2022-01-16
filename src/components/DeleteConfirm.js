c6db1c2bc154d7a5ecbc4b0813e194f37662c4d5
Zuzanna Usakiewicz <z.usakiewicz@gmail.com>
2022-01-16 16:56:32 +0100

    modify delete confirm component look

--- src/components/DeleteConfirm.js
+++ src/components/DeleteConfirm.js
@@ -1,14 +1,33 @@
 import React, { useContext } from "react";
 import { BooksContext } from "../contexts/BooksContext";
+import "../styles/DeleteConfirm.css";
+import "../styles/BookForm.css";
+import { BiX } from "react-icons/bi";
 
 export default function DeleteConfirm({ toggleValue, book }) {
   const { deleteBook } = useContext(BooksContext);
 
   return (
-    <div>
-      Do you want to confirm?
-      <button onClick={() => deleteBook(book.id)}>yes</button>
-      <button onClick={() => toggleValue(false)}>no</button>
+    <div className="confirm-container">
+      <h3>Do you want to delete this book? </h3>
+      <h5>Warning: This action cannot be undone!</h5>
+      <div>
+        <button
+          onClick={() => toggleValue(false)}
+          className="confirm-btn reject"
+        >
+          cancel
+        </button>
+        <button
+          onClick={() => deleteBook(book.id)}
+          className="confirm-btn confirm"
+        >
+          yes, delete it
+        </button>
+      </div>
+      <button onClick={() => toggleValue(false)} className="close-btn">
+        <BiX />
+      </button>
     </div>
   );
 }
